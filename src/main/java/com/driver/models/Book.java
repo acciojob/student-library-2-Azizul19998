41b8c9f1e8116f7130075a2c033b431dadf45854
Azizul <you@example.comazizuljohn121454@gmail.com>
2022-12-04 01:30:00 +0530

    fourth commit

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -45,17 +45,16 @@ public class Book {
     public Book() {
     }
 
-    public Book(String name, Genre genre, boolean available) {
-        this.name = name;
-        this.genre = genre;
-        this.available = available;
-    }
-//
-//    public Book(String name, Genre genre, boolean available, Author author) {
+//    public Book(String name, Genre genre, boolean available) {
 //        this.name = name;
 //        this.genre = genre;
 //        this.available = available;
-//        this.author = author;
 //    }
+//
+    public Book(String name, Genre genre,Author author) {
+        this.name = name;
+        this.genre = genre;
+        this.author = author;
+    }
 }
 
